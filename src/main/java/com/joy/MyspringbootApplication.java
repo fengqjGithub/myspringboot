316aec0bbc95d4d678cc0ea4fa328512bda5aa15
ame <409902978@qq.com>
2018-05-07 08:55:26 +0800

    tomcat打包

--- src/main/java/com/joy/MyspringbootApplication.java
+++ src/main/java/com/joy/MyspringbootApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,7 +17,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ServletComponentScan
 @EnableCaching
 @EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
-public class MyspringbootApplication  {
+public class MyspringbootApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyspringbootApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MyspringbootApplication.class, args);
     }
